f0eda2877927dfb9b9a8371863d52f836f2de923
mbelmonte <michael.belmonte@stonybrook.edu>
2022-02-15 14:37:48 -0500

    first changes

---
+++ homework2/prettyprint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+count=$2
+
+if [ $count -eq -1 ]
+then
+  count=${#1}
+fi
+
+if [ $# -eq 2 ]
+then
+  for i in $(seq 0 ${count})
+  do
+    echo "${1:$i:${#1}}"
+  done
+else
+  echo "Please include 2 arguments. For example: ./prettyprin.sh text 1"
+fi
--- images/imageconvert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in $(find . -name "*.jpg")
-do
-convert -resize 10% $file new$(basename $file .jpg).png
-done
